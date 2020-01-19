d92d6653ee1c5a2e28b9afacf3e509de9a28e64a
Arkadiusz Zasina <34420693+217868@users.noreply.github.com>
2020-01-19 18:01:42 +0100

    QuizResultActivity details

--- app/src/main/java/com/example/univeristyligthhousekeeper/ui/main/QuizResultActivity.java
+++ app/src/main/java/com/example/univeristyligthhousekeeper/ui/main/QuizResultActivity.java
@@ -5,19 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.univeristyligthhousekeeper.DatabaseModel.DatabaseAccess;
+import com.example.univeristyligthhousekeeper.DatabaseModel.Kierunek;
 import com.example.univeristyligthhousekeeper.R;
+
+import java.util.List;
 
 public class QuizResultActivity extends AppCompatActivity {
 
     TextView resultTextView;
+    DatabaseAccess databaseAccess;
+    List<Kierunek> kierunki;
+    Kierunek selectedKierunek;
+    String resultString;
+    TextView kierunekDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_result);
 
-        resultTextView = findViewById(R.id.resultTextView);
+        databaseAccess = DatabaseAccess.getInstance(getApplicationContext());
+        resultString = getIntent().getStringExtra("RESULT");
 
-        resultTextView.setText(getIntent().getStringExtra("RESULT"));
+        databaseAccess.open();
+        kierunki = databaseAccess.getKierunki();
+        databaseAccess.close();
+        for (Kierunek k: kierunki) {
+            if (resultString.contains(k.getKierunek())) selectedKierunek = k;
+        }
+
+        resultTextView = findViewById(R.id.resultTextView);
+        kierunekDescriptionTextView = findViewById(R.id.kierunekDescriptionTextView);
+        if (selectedKierunek != null)
+            kierunekDescriptionTextView.setText(selectedKierunek.getOpisKierunku());
+        resultTextView.setText(resultString);
     }
 }
